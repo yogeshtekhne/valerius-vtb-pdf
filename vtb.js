3484ec2ffc87ee5a5adbf3d5d16688471fac223b
Yogesh Kumar Sahu <yogesh@tekhne.nl>
2020-04-29 11:16:02 +0530

    code cleanup

--- vtb.js
+++ vtb.js
@@ -165,12 +165,16 @@ const customTransforms = {
   },
   'getBookedBy':(obj, params) => {
 
+    obj.dst.contactName = obj.dst.contactImage = obj.dst.contactEmail = obj.dst.contactText = '';
+
+    if (!obj.dst.TSOrder.bookedByInfo) return;
+
     let contactSignature = obj.dst.TSOrder.bookedByInfo.signature;
 
     obj.dst.contactName = obj.dst.TSOrder.bookedByInfo.name + ' ' + obj.dst.TSOrder.bookedByInfo.surname;
     obj.dst.contactImage = contactSignature.match(/<image>(.*?)<\/image>/)[1];
     obj.dst.contactEmail = contactSignature.match(/<contact>(.*?)<\/contact>/)[1];
-    obj.dst.contactText= contactSignature.match(/<text>(.*?)<\/text>/)[1];
+    obj.dst.contactText = contactSignature.match(/<text>(.*?)<\/text>/)[1];
 
     return obj;
   },
